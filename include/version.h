b0baef9a9ec9957ed63dfcba5c1ada5834c0ee69
ermiry <erick.salas@ermiry.com>
2021-08-23 23:36:20 -0500

    Added base version methods

---
+++ include/version.h
@@ -0,0 +1,19 @@
+#ifndef _NIGHT_VERSION_H_
+#define _NIGHT_VERSION_H_
+
+#define NIGHT_VERSION			"0.1"
+#define NIGHT_VERSION_NAME		"Version 0.1"
+#define NIGHT_VERSION_DATE		"17/07/2021"
+#define NIGHT_VERSION_TIME		"17:03 CST"
+#define NIGHT_VERSION_AUTHOR		"Erick Salas"
+
+// print full night version information
+extern void night_version_print_full (void);
+
+// print the version id
+extern void night_version_print_version_id (void);
+
+// print the version name
+extern void night_version_print_version_name (void);
+
+#endif
